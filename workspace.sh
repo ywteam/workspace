fe18ded2cee8d193546e96d5dc36e4af1f86ead8
Raphael C Rego <raphaelcarlosr@gmail.com>
2024-05-27 00:23:03 +0200

    chore: Refactor workspace setup script and remove unused submodules
    This commit refactors the workspace setup script to improve readability and maintainability. It also removes the unused submodules for the YDK projects, including shell, go, node, dotnet, and python, as well as the submodules for infra, public, docs, and private repositories.
    
    Note: This commit message does not include any meta information like issue references, tags, or author names.

--- workspace.sh
+++ workspace.sh
@@ -21,13 +21,18 @@ ydk:workspace:setup(){
             ! [[ "${CONFIG_KEY}" =~ ^submodule/([a-zA-Z0-9_]+)* ]] && continue
             local SUBMODULE_PATH="${CONFIG_KEY//submodule\//}"
             local SUBMODULE_REPO="${YDK_WKSPC_SETUP_CONFIG["${CONFIG_KEY}"]}"
+            
             if [[ -d "${SUBMODULE_PATH}" ]] && [[ -d ".git/modules/${SUBMODULE_PATH}" ]]; then
-                echo "Updating submodule ${SUBMODULE_PATH} ${SUBMODULE_REPO}"
-                git submodule update --remote "${SUBMODULE_PATH}"
-                continue
+                # git submodule deinit "${SUBMODULE_PATH}"
+                # git rm --cached "${SUBMODULE_PATH}"
+                # rm -rf .git/modules/"${SUBMODULE_PATH}"
+                rm -rf "${SUBMODULE_PATH}"
+                # echo "Updating submodule ${SUBMODULE_PATH} ${SUBMODULE_REPO}"
+                # git submodule update --remote "${SUBMODULE_PATH}"
+                # continue
             fi
-            echo "Adding submodule ${SUBMODULE_PATH} ${SUBMODULE_REPO}"
-            git submodule add "${SUBMODULE_REPO}" "${SUBMODULE_PATH}"            
+            # echo "Adding submodule ${SUBMODULE_PATH} ${SUBMODULE_REPO}"
+            # git submodule add "${SUBMODULE_REPO}" "${SUBMODULE_PATH}"            
         done
         # IFS=' ' read -r -a WORKSPACE_PATHS <<<"${YDK_WKSPC_SETUP_CONFIG["paths"]}"
         # for WORKSPACE_PATH in "${WORKSPACE_PATHS[@]}"; do
