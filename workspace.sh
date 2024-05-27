ee072085125bfd752bbe44eaa3e4d7ef31d1228f
Raphael C Rego <raphaelcarlosr@gmail.com>
2024-05-27 17:53:12 +0200

    Refactor workspace setup script and remove unused submodules

--- workspace.sh
+++ workspace.sh
@@ -24,13 +24,13 @@ ydk:workspace:setup(){
             ! [[ "${CONFIG_KEY}" =~ ^submodule/([a-zA-Z0-9_]+)* ]] && continue
             local SUBMODULE_PATH="${CONFIG_KEY//submodule\//}"
             local SUBMODULE_REPO="${YDK_WKSPC_SETUP_CONFIG["${CONFIG_KEY}"]}"
-            # git submodule deinit "${SUBMODULE_PATH}"
-            # git rm -rf --cached "${SUBMODULE_PATH}"
-            # rm -rf .git/modules/"${SUBMODULE_PATH}"
-            # rm -rf "${SUBMODULE_PATH}"
-            # git config -f .gitmodules --remove-section "submodule.${SUBMODULE_PATH}"
-            # git config --local --remove-section "submodule.${SUBMODULE_PATH}"
-            # continue
+            git submodule deinit "${SUBMODULE_PATH}"
+            git rm -rf --cached "${SUBMODULE_PATH}"
+            rm -rf .git/modules/"${SUBMODULE_PATH}"
+            rm -rf "${SUBMODULE_PATH}"
+            git config -f .gitmodules --remove-section "submodule.${SUBMODULE_PATH}"
+            git config --local --remove-section "submodule.${SUBMODULE_PATH}"
+            continue
             if [[ -d "${SUBMODULE_PATH}" ]] && [[ -d ".git/modules/${SUBMODULE_PATH}" ]]; then                
                 echo "Updating submodule ${SUBMODULE_PATH} ${SUBMODULE_REPO}"
                 git submodule update --remote "${SUBMODULE_PATH}"
